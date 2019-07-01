7647ef105107648330d5b48167f22efc2a747922
Somesh Singh <someshsingh9414@gmail.com>
2019-07-01 21:04:08 +0530

    First Format

--- Format.py
+++ Format.py
@@ -1,2 +1,19 @@
-Text_Raw = open('Dataset/cano.txt').read()
-print(Text_Raw)
+import re
+import os
+
+Data_Path='Dataset/cano.txt'
+Write_Path=Data_Path[:8]+"Clean/"+Data_Path[8:]
+
+def cleaner(file_path):
+  text = open(file_path).read()
+  text=re.sub(r'\n+', '\n',text).strip()
+  text=re.sub(' +', ' ',text).strip()
+  return text
+
+Data=cleaner(Data_Path)
+if not os.path.exists:
+	os.makedirs(Write_Path[:14])
+
+Clean_File=open(Write_Path,"w")
+Clean_File.writelines(Data)
+Clean_File.close()
