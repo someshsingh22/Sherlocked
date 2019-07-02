e4a837d6712502d41b9ed3c51c2b45e2c2d388e9
Somesh Singh <someshsingh9414@gmail.com>
2019-07-02 22:40:10 +0530

    Comments added

--- Format.py
+++ Format.py
@@ -1,19 +1,22 @@
+#imports
 import re
 import os
 
+#set path for file
 Data_Path='Dataset/cano.txt'
 Write_Path=Data_Path[:8]+"Clean/"+Data_Path[8:]
 
+#clean file
 def cleaner(file_path):
   text = open(file_path).read()
   text=re.sub(r'\n+', '\n',text).strip()
   text=re.sub(' +', ' ',text).strip()
   return text
 
+#write and save it in clean folder
 Data=cleaner(Data_Path)
 if not os.path.exists:
 	os.makedirs(Write_Path[:14])
-
 Clean_File=open(Write_Path,"w")
 Clean_File.writelines(Data)
 Clean_File.close()
